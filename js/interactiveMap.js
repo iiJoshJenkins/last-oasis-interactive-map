39809c92fa59d595ac7164f97f3963068708241c
Josh Jenkins <jenkins@topscreenmedia.com>
2020-04-16 13:41:22 +0100

    added standard map image as background

---
+++ js/interactiveMap.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("map");
+var context = canvas.getContext("2d");
+var imageObj = new Image();
+
+imageObj.onload = function () {
+  context.drawImage(imageObj, 0, 0);
+};
+imageObj.src = "../images/interactiveMap.png";
