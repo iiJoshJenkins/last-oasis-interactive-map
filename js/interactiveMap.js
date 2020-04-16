e963b6adfb69b95a024ee4da59fb3a73c6746060
Josh Jenkins <jenkins@topscreenmedia.com>
2020-04-16 13:58:07 +0100

    added styles for fullscreen

--- js/interactiveMap.js
+++ js/interactiveMap.js
@@ -1,8 +1,13 @@
 window.onload = () => {
   var canvas = document.getElementById("map");
   var context = canvas.getContext("2d");
+
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
   var imageObj = new Image();
   imageObj.src = "./images/interactiveMap.png";
+
   imageObj.onload = function () {
     context.drawImage(imageObj, 0, 0);
   };
