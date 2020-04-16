64e58cacf24dcb5a7410df83d650081daedd5e06
Josh Jenkins <jenkins@topscreenmedia.com>
2020-04-16 13:49:46 +0100

    fixed file path issue

--- js/interactiveMap.js
+++ js/interactiveMap.js
@@ -2,9 +2,8 @@ window.onload = () => {
   var canvas = document.getElementById("map");
   var context = canvas.getContext("2d");
   var imageObj = new Image();
-
+  imageObj.src = "./images/interactiveMap.png";
   imageObj.onload = function () {
     context.drawImage(imageObj, 0, 0);
   };
-  imageObj.src = "../images/interactiveMap.png";
 };
