4056926fe0ca84a81615eb6ec3000505a45457ae
Josh Jenkins <jenkins@topscreenmedia.com>
2020-04-16 13:45:23 +0100

    added onload for performance

--- js/interactiveMap.js
+++ js/interactiveMap.js
@@ -1,8 +1,10 @@
-var canvas = document.getElementById("map");
-var context = canvas.getContext("2d");
-var imageObj = new Image();
+window.onload = () => {
+  var canvas = document.getElementById("map");
+  var context = canvas.getContext("2d");
+  var imageObj = new Image();
 
-imageObj.onload = function () {
-  context.drawImage(imageObj, 0, 0);
+  imageObj.onload = function () {
+    context.drawImage(imageObj, 0, 0);
+  };
+  imageObj.src = "../images/interactiveMap.png";
 };
-imageObj.src = "../images/interactiveMap.png";
